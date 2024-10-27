89acd25017f0433002d4710aba7a6cfb2bf6fd06
nanosando <mrryu006@gmail.com>
2024-10-27 20:46:39 +0900

    Add: Locations

--- src/resource/fish.js
+++ src/resource/fish.js
@@ -1,3 +1,4 @@
+// Fish Images
 import fish0 from './fish/0.png';
 import fish1 from './fish/1.png';
 import fish2 from './fish/2.png';
@@ -46,9 +47,32 @@ import fish37 from './fish/37.png';
 import fish38 from './fish/38.png';
 import fish39 from './fish/39.png';
 
-
+// Map Images
 import loc6 from './location/6.png';
 import loc8 from './location/8.png';
+import loc9 from './location/9.png';
+
+import loc10 from './location/10.png';
+import loc11 from './location/11.png';
+import loc12 from './location/12.png';
+import loc13 from './location/13.png';
+import loc14 from './location/14.png';
+import loc15 from './location/15.png';
+
+import loc20 from './location/20.png';
+import loc24 from './location/24.png';
+import loc25 from './location/25.png';
+import loc26 from './location/26.png';
+import loc27 from './location/27.png';
+import loc28 from './location/28.png';
+
+import loc34 from './location/34.png';
+import loc35 from './location/35.png';
+import loc36 from './location/36.png';
+import loc37 from './location/37.png';
+import loc38 from './location/38.png';
+import loc39 from './location/39.png';
+
 
 export default [
     {'name': '블루탱', 'img': fish0, 'loc': '초입'},
@@ -61,42 +85,41 @@ export default [
     {'name': '달걀프라이', 'img': fish6, 'loc': '초입', 'loc_map': loc6},
     {'name': '꺼끌복', 'img': fish7, 'loc': '초입'},
     {'name': '버팔로피쉬', 'img': fish8, 'loc': '초입', 'loc_map': loc8},
-    {'name': '백기흉상어', 'img': fish9, 'loc': '초입'},
+    {'name': '백기흉상어', 'img': fish9, 'loc': '초입', 'loc_map': loc9},
 
-    {'name': '귀상어', 'img': fish10, 'loc': '중간수역'},
-    {'name': '무태상어', 'img': fish11, 'loc': '초입'},
-    {'name': '황다랑어', 'img': fish12, 'loc': '초입'},
-    {'name': '참다랑어', 'img': fish13, 'loc': '초입'},
-    {'name': '청새치', 'img': fish14, 'loc': '초입'},
+    {'name': '귀상어', 'img': fish10, 'loc': '중간수역', 'loc_map': loc10},
+    {'name': '무태상어', 'img': fish11, 'loc': '초입', 'loc_map': loc11},
+    {'name': '황다랑어', 'img': fish12, 'loc': '초입', 'loc_map': loc12},
+    {'name': '참다랑어', 'img': fish13, 'loc': '초입', 'loc_map': loc13},
+    {'name': '청새치', 'img': fish14, 'loc': '초입', 'loc_map': loc14},
 
-    {'name': '환도상어', 'img': fish15, 'loc': '초입'},
+    {'name': '환도상어', 'img': fish15, 'loc': '초입', 'loc_map': loc15},
     {'name': '무당씬벵이', 'img': fish16, 'loc': '중간수역'},
     {'name': '청줄바리', 'img': fish17, 'loc': '중간수역'},
     {'name': '전갱이', 'img': fish18, 'loc': '중간수역'},
     {'name': '큰꼬치고기', 'img': fish19, 'loc': '중간수역'},
 
-    {'name': '흰점박이', 'img': fish20, 'loc': '중간수역'},
+    {'name': '흰점박이', 'img': fish20, 'loc': '중간수역', 'loc_map': loc20},
     {'name': '무늬바리', 'img': fish21, 'loc': '중간수역'},
     {'name': '머구리능어', 'img': fish22, 'loc': '중간수역'},
     {'name': '대서양아귀', 'img': fish23, 'loc': '중간수역'},
-    {'name': '훔볼트오징어', 'img': fish24, 'loc': '중간수역'},
+    {'name': '훔볼트오징어', 'img': fish24, 'loc': '중간수역', 'loc_map': loc24},
 
-    {'name': '톱상어', 'img': fish25, 'loc': '중간수역'},
-    {'name': '지브라상어', 'img': fish26, 'loc': '중간수역'},
-    {'name': '돛새치', 'img': fish27, 'loc': '중간수역'},
-    {'name': '감투빗', 'img': fish28, 'loc': '심해'},
+    {'name': '톱상어', 'img': fish25, 'loc': '중간수역', 'loc_map': loc25},
+    {'name': '지브라상어', 'img': fish26, 'loc': '중간수역', 'loc_map': loc26},
+    {'name': '돛새치', 'img': fish27, 'loc': '중간수역', 'loc_map': loc27},
+    {'name': '감투빗', 'img': fish28, 'loc': '심해', 'loc_map': loc28},
     {'name': '점씬벵이', 'img': fish29, 'loc': '심해'},
 
     {'name': '금눈돔', 'img': fish30, 'loc': '심해'},
     {'name': '덤보문어', 'img': fish31, 'loc': '심해'},
     {'name': '블롭피쉬', 'img': fish32, 'loc': '심해'},
     {'name': '귀신고기', 'img': fish33, 'loc': '심해'},
-    {'name': '쿠키커터상어', 'img': fish34, 'loc': '심해'},
+    {'name': '쿠키커터상어', 'img': fish34, 'loc': '심해', 'loc_map': loc34},
 
-    {'name': '주름상어', 'img': fish35, 'loc': '심해'},
-    {'name': '코은상어', 'img': fish36, 'loc': '심해'},
-    {'name': '메가마우스', 'img': fish37, 'loc': '심해'},
-    {'name': '피아누스(좌)', 'img': fish38, 'loc': '심해'},
-    {'name': '피아누스(우)', 'img': fish39, 'loc': '심해'},
-
+    {'name': '주름상어', 'img': fish35, 'loc': '심해', 'loc_map': loc35},
+    {'name': '코은상어', 'img': fish36, 'loc': '심해', 'loc_map': loc36},
+    {'name': '메가마우스', 'img': fish37, 'loc': '심해', 'loc_map': loc37},
+    {'name': '피아누스(좌)', 'img': fish38, 'loc': '심해', 'loc_map': loc38},
+    {'name': '피아누스(우)', 'img': fish39, 'loc': '심해', 'loc_map': loc39},
 ];
--- src/resource/location.js
+++
@@ -1,2 +0,0 @@
-import loc6 from './location/6.png';
-import loc8 from './location/8.png';
